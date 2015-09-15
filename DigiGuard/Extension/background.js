b8ce5b81f6d2fb4c50f7ae87f8f711cf4e61dae9
dor <amirdor@gmail.com>
2015-09-15 12:27:06 +0300

    auto check

--- DigiGuard/Extension/background.js
+++ DigiGuard/Extension/background.js
@@ -1,28 +1,28 @@
 document.onreadystatechange = function () {
-	//When document is fully loaded
-	try{
-		chrome.storage.local.get('auto', function (result) {
-			var auto = result.auto;
-			//alert(auto);
-		});
-		if (document.readyState == "complete") {
-			//Add the json word file
-			var words = strWords;
-			var count = 0;
-			//Search each word in the document
-			for (var i=0;i<words.length;i++){
-				if (window.find(words[i])){
-					count++;
-				}
-			}
-			if (count >3){
-				//Add Ajax call here
-				//Send document.documentElement.innerHTML
-			}
-		//	});
-		}
-	}
-	catch(err) {
-		alert(err.message);
-	}
+    //When document is fully loaded
+    try{
+
+        if (document.readyState == "complete") {
+            var auto = localStorage["AutoCheck"];
+            if (auto=="true"){
+                //Add the json word file
+                var words = strWords;
+                var count = 0;
+                //Search each word in the document
+                for (var i=0;i<words.length;i++){
+                    if (window.find(words[i])){
+                        count++;
+                    }
+                }
+                if (count >3){
+                    //Add Ajax call here
+                    //Send document.documentElement.innerHTML
+                }
+                //	});
+            }
+        }
+    }
+    catch(err) {
+        alert(err.message);
+    }
 }
